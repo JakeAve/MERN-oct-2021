35c6e4c1d10b9feebb71af6f3c78fd113e1883b1
Waldo Villagran <85133363+waldo-dev@users.noreply.github.com>
2021-12-28 19:50:18 -0300

    cambios de waldo

--- full-stack/administrador-de-productos/client/src/components/ProductDetalles/ProductDetalles.js
+++ full-stack/administrador-de-productos/client/src/components/ProductDetalles/ProductDetalles.js
@@ -3,12 +3,18 @@ import { Link } from "@reach/router";
 
 const Productdetalles = (props) => {
   const { _id, title, price, description } = props;
+  
+  const onClick = () => {
+    
+  }
+
   return (
     <div className="product-detalles">
       <h2>{title}</h2>
       <p>${Number(price).toFixed(2)}</p>
       <p>{description}</p>
       <Link to={`/edit/${_id}`}>Edit</Link>
+      <button onClick={onClick}>Eliminar</button>
     </div>
   );
 };
